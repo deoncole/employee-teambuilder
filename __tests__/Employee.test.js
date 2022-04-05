62baa180273039eef3ae6be5e9ee326fa5ea2765
Cole <deoncole77@gmail.com>
2022-04-04 22:02:44 -0400

    Manager test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,7 @@
 // require the Employee js file for testing
 const Employee = require('../lib/Employee');
 
+// test to get the emplyoee's name
 test("gets the employee's name", () => {
     const empName = new Employee().getName();
     const employee2 = new Employee('Vegeta', '1', 'dbz@email.com');
@@ -8,6 +9,7 @@ test("gets the employee's name", () => {
     expect(empName).toEqual(employee2.name);
   });
 
+  // test to get the empployee's id
   test("gets the employee's ID", () => {
     const empId = new Employee().getId();
     const employee2 = new Employee('Vegeta', '1', 'dbz@email.com');
@@ -16,6 +18,7 @@ test("gets the employee's name", () => {
     
   });
 
+  // test to get the employee's email
   test("gets the employee's email", () => {
     const empEmail = new Employee().getEmail();
     const employee2 = new Employee('Vegeta', '1', 'dbz@email.com');
@@ -23,6 +26,7 @@ test("gets the employee's name", () => {
     expect(empEmail).toEqual(employee2.email);
   });
 
+  // test to get the employee's role
   test("gets the employee's role", () => {
     const empRole = new Employee().getRole();
   
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,40 +1,45 @@
 // require the Manager js file for testing
 const Manager = require('../lib/Manager');
 
+// test to check that the manager's name is inherited from the employee class
 test("get's the Manager's name", () => {
-    const managerName = new Manager().getName;
+    const managerName = new Manager().getName();
     const manager2 = new Manager('Vegeta', '1', 'dbz@email.com','1234567890');
 
     expect(managerName).toEqual(manager2.name);
 
 });
 
+// test to check that the manager's ID is inherited from the employee class
 test("get's the Manager's ID", () => {
-    const managerId = new Manager().getId;
+    const managerId = new Manager().getId();
     const manager2 = new Manager('Vegeta', '1', 'dbz@email.com','1234567890');
 
     expect(managerId).toEqual(manager2.id);
 
 });
 
+// test to check that the manager's email is inherited from the employee class
 test("get's the Manager's email", () => {
-    const managerEmail = new Manager().getEmail;
+    const managerEmail = new Manager().getEmail();
     const manager2 = new Manager('Vegeta', '1', 'dbz@email.com','1234567890');
 
-    expect(managerEmail).toEqual(manager2.id);
+    expect(managerEmail).toEqual(manager2.email);
 
 });
 
+// test to get the manager's office number
 test("get's the Manager's office number", () => {
-    const managerNumber = new Manager() ;
+    const managerNumber = new Manager().getOfficeNumber();
     const manager2 = new Manager('Vegeta', '1', 'dbz@email.com','1234567890');
 
-    expect(managerNumber.officeNumber).toEqual(manager2.officeNumber);
+    expect(managerNumber).toEqual(manager2.officeNumber);
 
 });
 
+// test to get the manager's role
 test("gets the Manager's role", () => {
     const managerRole = new Manager().getRole();
-    // expect(managerRole).toEqual('Manager');
+    expect(managerRole).toEqual('Manager');
     
 });
